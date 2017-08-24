6476af4e6effe95129af75d565b6ab28d388569a
Leonardo Andres <leonardoandres.1298@gmail.com>
2017-08-24 18:13:53 -0400

    Link and LinkList files added. Querying from graphql

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
+import LinkList from './LinkList';
 import '../styles/App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      </div>
+      <LinkList />
     );
   }
 }
